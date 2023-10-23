8205ad41686976f821e84c787a157183c1af6d2f
Aaroco <cohenaronee@gmail.com>
2023-10-23 17:11:24 +0200

    COMMIT2

--- src/main/java/sio/bulletin/BulletinController.java
+++ src/main/java/sio/bulletin/BulletinController.java
@@ -11,12 +11,10 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-import java.util.TreeMap;
 
-public class BulletinController implements Initializable
-{
+public class BulletinController implements Initializable {
     DecimalFormat df;
-    HashMap<String,HashMap<String, HashMap<String,ArrayList<Etudiant>>>> lesBulletins;
+    HashMap<String, HashMap<String, ArrayList<Etudiant>>> lesBulletins;
     @FXML
     private AnchorPane apBulletin;
     @FXML
@@ -47,33 +45,79 @@ public class BulletinController implements Initializable
     private TextField txtNoteMaxi;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle)
-    {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
         apBulletin.toFront();
         df = new DecimalFormat("#.##");
-        lesBulletins = new HashMap<>();
-        lvMatieres.getItems().addAll("Maths","Anglais","Economie");
-        lvDevoirs.getItems().addAll("Devoir n°1","Devoir n°2","Devoir n°3","Devoir n°4");
-        cboTrimestres.getItems().addAll("Trim 1","Trim 2","Trim 3");
+        lesBulletins = new HashMap<String, HashMap<String, ArrayList<Etudiant>>>();
+        lvMatieres.getItems().addAll("Maths", "Anglais", "Economie");
+        lvDevoirs.getItems().addAll("Devoir n°1", "Devoir n°2", "Devoir n°3", "Devoir n°4");
+        cboTrimestres.getItems().addAll("Trim 1", "Trim 2", "Trim 3");
         cboTrimestres.getSelectionModel().selectFirst();
     }
 
     @FXML
-    public void btnMenuClicked(Event event)
-    {
-        if(event.getSource()==btnMenuBulletin)
-        {
+    public void btnMenuClicked(Event event) {
+        if (event.getSource() == btnMenuBulletin) {
             apBulletin.toFront();
-        }
-        else if(event.getSource()==btnMenuMoyenne)
-        {
+        } else if (event.getSource() == btnMenuMoyenne) {
             apMoyenne.toFront();
         }
     }
 
     @FXML
-    public void btnValiderClicked(Event event)
-    {
+    public void btnValiderClicked(Event event) {
+        if (lvMatieres.getSelectionModel().getSelectedItem() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Choix de la Matière");
+            alert.setHeaderText("");
+            alert.setContentText("Sélectionner une matiere");
+            alert.showAndWait();
+        } else if (lvDevoirs.getSelectionModel().getSelectedItem() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Choix du Devoir");
+            alert.setHeaderText("");
+            alert.setContentText("Sélectionner un Devoir");
+            alert.showAndWait();
+        } else if (txtNomEtudiant.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Choix d'un étudiant");
+            alert.setHeaderText("");
+            alert.setContentText("Saisir un étudiant");
+            alert.showAndWait();
+        } else if (txtNote.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Choix d'une note");
+            alert.setHeaderText("");
+            alert.setContentText("Saisir une note");
+            alert.showAndWait();
+        } else {
+            Etudiant etudiant = new Etudiant(txtNomEtudiant.getText(), Integer.parseInt(txtNote.getText()));
+            if (!lesBulletins.containsKey(lvMatieres.getSelectionModel().getSelectedItem())) {
+                ArrayList<Etudiant> lesEtudiants = new ArrayList<>();
+                lesEtudiants.add(etudiant);
+
+
+                HashMap<String, ArrayList<Etudiant>> lesDevoirs = new HashMap<>();
+                lesDevoirs.put(lvDevoirs.getSelectionModel().getSelectedItem().toString(), lesEtudiants);
+                lesBulletins.put(lvMatieres.getSelectionModel().getSelectedItem().toString(), lesDevoirs);
+            } else if (!lesBulletins.get(lvMatieres.getSelectionModel().getSelectedItem().toString()).containsKey(lvDevoirs.getSelectionModel().getSelectedItem().toString())) {
+
+                ArrayList<Etudiant> lesDevoirs = new ArrayList<>();
+                lesDevoirs.add(etudiant);
+                lesBulletins.get(lvMatieres.getSelectionModel().getSelectedItem().toString()).put(lvDevoirs.getSelectionModel().getSelectedItem().toString(), lesDevoirs);
+
+
+            } else {
+                lesBulletins.get(lvMatieres.getSelectionModel().getSelectedItem().toString()).get(lvDevoirs.getSelectionModel().getSelectedItem().toString()).add(etudiant);
+            }
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Affectation réussi");
+            alert.setHeaderText("");
+            alert.setContentText("Montant enregistrer");
+            alert.show();
+
+
+        }
         // A vous de jouer
 
     }
@@ -83,5 +127,8 @@ public class BulletinController implements Initializable
     {
         // A vous de jouer
 
+
     }
 }
+
+
